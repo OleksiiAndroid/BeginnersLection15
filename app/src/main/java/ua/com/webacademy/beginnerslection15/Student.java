df795e7ce8b44b1ea75064ae61a2d9666f9c6167
Oleksii Ponomarov <spinne@ua.fm>
2019-08-20 14:50:00 +0300

    update api 29

--- app/src/main/java/ua/com/webacademy/beginnerslection15/Student.java
+++ app/src/main/java/ua/com/webacademy/beginnerslection15/Student.java
@@ -1,10 +1,16 @@
 package ua.com.webacademy.beginnerslection15;
 
-
 import android.os.Parcel;
 import android.os.Parcelable;
 
 public class Student implements Parcelable {
+    public static final String TABLE_NAME = "Students";
+
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_FIRST_NAME = "FirstName";
+    public static final String COLUMN_LAST_NAME = "LastName";
+    public static final String COLUMN_AGE = "Age";
+
     public long id;
 
     public String FirstName;
@@ -41,12 +47,19 @@ public class Student implements Parcelable {
     }
 
     public static final Creator<Student> CREATOR = new Creator<Student>() {
+        @Override
         public Student createFromParcel(Parcel source) {
             return new Student(source);
         }
 
+        @Override
         public Student[] newArray(int size) {
             return new Student[size];
         }
     };
+
+    @Override
+    public String toString() {
+        return String.format("id:%s, %s %s, age:%s", id, FirstName, LastName, Age);
+    }
 }
